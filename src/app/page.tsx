457da6123531ae368768dd4173555bea9f0a78fa
Sebastian Österberg <sebastian.osterberg@gmail.com>
2024-07-01 00:45:02 +0200

    Fix warnings and bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
         const url = new URL(story.url);
 
         return (
-          <div className="leading-none mb-4">
+          <div className="leading-none mb-4" key={story.id}>
             <a title={story.title} href={story.url} target="_blank" className="whitespace-nowrap text-ellipsis overflow-hidden block font-bold visited:text-slate-500">{index+1}. {story.title}</a>
             <div className="text-xs text-slate-700">{url.host} - <Link href={`/${story.id}`}>{story.score} poäng - {story.descendants || 'Inga'} kommentarer - {Math.floor(Date.now()/1000 - story.time)} sekunder sedan</Link></div>
           </div>
